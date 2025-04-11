6c3b1f82fe24055b3e36ab639ba5cd3800792aa3
Ujjaval Gupta <ujjavalgupta2000@gmail.com>
2025-04-11 11:58:18 +0530

    Add edit note feature

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { v4 as uuidV4 } from 'uuid';
 import NoteList from './NoteList';
 import { NoteLayout } from './NoteLayout';
 import { Note } from './Note';
+import { EditNote } from './EditNote';
 
 export type Note = {
   id: string;
@@ -103,7 +104,7 @@ function App() {
         <Route path='/new' element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />} />
         <Route path='/:id' element={<NoteLayout notes={notesWithTags}  />}>
           <Route index element={<Note onDelete={onDeleteNote} />} />
-          <Route path='edit' element={<h1>Edit</h1>} />
+          <Route path='edit' element={<EditNote onSubmit={onUpdateNote} onAddTag={addTag} availableTags={tags} />} />
         </Route>
         <Route path='*' element={<Navigate to='/' />} />
       </Routes>
